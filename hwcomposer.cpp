c5993e2deb942682c3a61ca561b9c82113aabfd1
jeff.yang <jeff.yang@amlogic.com>
2013-05-20 16:58:50 +0800

    PD #72549: modify the reboot issue

--- hwcomposer.cpp
+++ hwcomposer.cpp
@@ -19,16 +19,34 @@
 #include <hardware/hardware.h>
 
 #include <fcntl.h>
-#include <errno.h>
+#include <math.h>
+#include <poll.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/resource.h>
+
+#include <EGL/egl.h>
+
+#define HWC_REMOVE_DEPRECATED_VERSIONS 1
+
+#include <cutils/compiler.h>
 #include <cutils/log.h>
 #include <cutils/atomic.h>
 #include <cutils/properties.h>
 
+#include <utils/String8.h>
 #include <hardware/hwcomposer.h>
 
 #include <EGL/egl.h>
-
+#include <utils/Vector.h>
+#include <utils/Timers.h>
 // for private_handle_t
 #include "../../libhardware/modules/gralloc/gralloc_priv.h"
 
@@ -38,17 +56,24 @@
 #define LOGD ALOGD
 #endif
 
+extern "C" int clock_nanosleep(clockid_t clock_id, int flags,
+                           const struct timespec *request,
+                           struct timespec *remain);
 /*****************************************************************************/
+
 
 struct hwc_context_1_t {
     hwc_composer_device_1_t device;
     /* our private state goes below here */
-    hwc_layer_1_t const* saved_layer;
+		hwc_layer_1_t const* saved_layer;
     unsigned saved_transform;
     int saved_left;
     int saved_top;
     int saved_right;
     int saved_bottom;
+	int vsync_enable;
+    const hwc_procs_t       *procs;
+    pthread_t               vsync_thread;
 };
 
 static int hwc_device_open(const struct hw_module_t* module, const char* name,
@@ -71,6 +96,10 @@ hwc_module_t HAL_MODULE_INFO_SYM = {
         reserved : {0},
     }
 };
+
+static pthread_cond_t hwc_cond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t hwc_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 
 /*****************************************************************************/
 
@@ -153,7 +182,7 @@ static void hwc_overlay_compose(hwc_composer_device_1_t *dev, hwc_layer_1_t cons
     ctx->saved_bottom = l->displayFrame.bottom;
 }
 
-static void dump_layer(hwc_layer_t const* l) {
+/*static void dump_layer(hwc_layer_t const* l) {
     LOGD("\ttype=%d, flags=%08x, handle=%p, tr=%02x, blend=%04x, {%d,%d,%d,%d}, {%d,%d,%d,%d}",
             l->compositionType, l->flags, l->handle, l->transform, l->blending,
             l->sourceCrop.left,
@@ -164,7 +193,7 @@ static void dump_layer(hwc_layer_t const* l) {
             l->displayFrame.top,
             l->displayFrame.right,
             l->displayFrame.bottom);
-}
+}*/
 
 static int hwc_blank(struct hwc_composer_device_1* dev,
                      int disp,
@@ -178,7 +207,17 @@ static int hwc_eventControl(struct hwc_composer_device_1* dev,
                             int event,
                             int enabled)
 {
-    return 0;
+    struct hwc_context_1_t* ctx = (struct hwc_context_1_t *)dev;
+    switch (event) 
+    {
+        case HWC_EVENT_VSYNC:
+            ctx->vsync_enable =enabled;
+            pthread_mutex_lock(&hwc_mutex);
+            pthread_cond_signal(&hwc_cond);
+            pthread_mutex_unlock(&hwc_mutex);
+            return 0;    
+    }    
+	return -EINVAL;
 }
 
 static int hwc_prepare(struct hwc_composer_device_1 *dev,
@@ -210,23 +249,80 @@ static int hwc_set(struct hwc_composer_device_1 *dev,
         }
     }
 
-//TODO: revert this after ARM has official Mali driver release for Android 4.2
-#if 0
+
     EGLBoolean success = eglSwapBuffers(displays[0]->dpy, displays[0]->sur);
     if (!success) {
         return HWC_EGL_ERROR;
     }
-#endif
     return 0;
 }
 
 static int hwc_device_close(struct hw_device_t *dev)
 {
-    struct hwc_context_t* ctx = (struct hwc_context_t*)dev;
+    struct hwc_context_1_t* ctx = (struct hwc_context_1_t*)dev;
     if (ctx) {
         free(ctx);
     }
     return 0;
+}
+
+
+
+static void *hwc_vsync_thread(void *data)
+{
+    struct hwc_context_1_t* ctx = (struct hwc_context_1_t*)data;
+    nsecs_t nextFakeVSync = 0;
+
+    setpriority(PRIO_PROCESS, 0, HAL_PRIORITY_URGENT_DISPLAY-1);
+	sleep(2);
+
+    while (true) {
+        pthread_mutex_lock(&hwc_mutex);
+        while(ctx->vsync_enable==false)
+        {
+            pthread_cond_wait(&hwc_cond, &hwc_mutex);
+        }
+        pthread_mutex_unlock(&hwc_mutex);
+
+        const nsecs_t period = 20000000; //50Hz
+        const nsecs_t now = systemTime(CLOCK_MONOTONIC);
+        nsecs_t next_vsync = nextFakeVSync;
+        nsecs_t sleep = next_vsync - now;
+        if (sleep < 0) {
+            // we missed, find where the next vsync should be
+            sleep = (period - ((now - next_vsync) % period));
+            next_vsync = now + sleep;
+        }
+        nextFakeVSync = next_vsync + period;
+
+        struct timespec spec;
+        spec.tv_sec  = next_vsync / 1000000000;
+        spec.tv_nsec = next_vsync % 1000000000;
+
+        int err;
+        do {
+            err = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &spec, NULL);
+        } while (err<0 && errno == EINTR);
+
+        if (err == 0) {
+            if (ctx->procs) {
+                ctx->procs->vsync(ctx->procs, 0, next_vsync);
+            }
+        }
+    }
+
+    return NULL;
+}
+
+
+
+static void hwc_registerProcs(hwc_composer_device_1_t *dev,
+            hwc_procs_t const* procs)
+{
+    struct hwc_context_1_t* ctx = (struct hwc_context_1_t*)dev;
+    if (ctx) {
+        ctx->procs = procs;
+    }
 }
 
 /*****************************************************************************/
@@ -250,11 +346,17 @@ static int hwc_device_open(const struct hw_module_t* module, const char* name,
 
         dev->device.blank = hwc_blank;
         dev->device.eventControl = hwc_eventControl;
+        dev->device.registerProcs = hwc_registerProcs;
         dev->device.prepare = hwc_prepare;
         dev->device.set = hwc_set;
-
+        dev->vsync_enable = false;
         *device = &dev->device.common;
         status = 0;
+        
+        status = pthread_create(&dev->vsync_thread, NULL, hwc_vsync_thread, dev);
+        if (status) {
+            ALOGE("failed to start vsync thread: %s", strerror(status));
+        }
     }
     return status;
 }
