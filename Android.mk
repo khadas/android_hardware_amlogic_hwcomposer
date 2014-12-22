dbe67452363e03a5af3c1cb887c970ece6ea28dc
Lawrence Mok <lawrence.mok@amlogic.com>
2014-12-22 11:52:02 -0800

    update LibPlayer path to vendor/amlogic/frameworks/av/
    Change-Id: I40e1b15b798b4abebb3fb1108221fd1cb1b72dc9

--- Android.mk
+++ Android.mk
@@ -27,7 +27,7 @@ LOCAL_C_INCLUDES += system/core/libion/include/ \
 
 ifneq ($(WITH_LIBPLAYER_MODULE),false)
 LOCAL_SHARED_LIBRARIES += libamavutils_alsa
-AMPLAYER_APK_DIR=$(TOP)/packages/amlogic/LibPlayer/
+AMPLAYER_APK_DIR=$(TOP)/vendor/amlogic/frameworks/av/LibPlayer/
 LOCAL_C_INCLUDES += \
     $(AMPLAYER_APK_DIR)/amavutils/include
 LOCAL_CFLAGS += -DWITH_LIBPLAYER_MODULE=1
