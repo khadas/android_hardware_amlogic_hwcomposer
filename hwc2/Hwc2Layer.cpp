e6744bc6f8d4c77425ba9751af74f0c0bf4ff14e
shuze.ma <shuze.ma@amlogic.com>
2018-12-19 15:10:20 +0800

    hwc2: fix dim layer crash [1/1]
    PD#SWPL-3385
    
    Problem:
    Missing detection of invalid buffer.
    
    Solution:
    Add exception protection.
    
    Verify:
    Verify on p212
    
    Test:
    A-55
    Run Peanut stress test for 12 hours with the following
    settings: "10 playbacks - max 10 min each - repeat forever"
    
    Change-Id: Id21b6f1f11236f2ce46e0714d78af17f705bca63
    Signed-off-by: shuze.ma <shuze.ma@amlogic.com>

--- hwc2/Hwc2Layer.cpp
+++ hwc2/Hwc2Layer.cpp
@@ -24,6 +24,11 @@ Hwc2Layer::~Hwc2Layer() {
 
 hwc2_error_t Hwc2Layer::handleDimLayer(buffer_handle_t buffer) {
     int bufFd = am_gralloc_get_buffer_fd(buffer);
+    if (bufFd < 0) {
+        MESON_LOGE("[%s]: get invalid buffer fd %d", __func__, bufFd);
+        return HWC2_ERROR_NONE;
+    }
+
     int bufFormat = am_gralloc_get_format(buffer);
 
     /* Number of pixel components in memory
@@ -47,45 +52,48 @@ hwc2_error_t Hwc2Layer::handleDimLayer(buffer_handle_t buffer) {
             break;
     }
     char *base = (char *)mmap(NULL, components, PROT_READ, MAP_SHARED, bufFd, 0);
-    int color[components];
-    memcpy(color, base, components);
+    if (base != MAP_FAILED) {
+        int color[components];
+        memcpy(color, base, components);
 
-    switch (bufFormat)
-    {
-        case HAL_PIXEL_FORMAT_BGRA_8888:
-            mColor.b = color[0];
-            mColor.g = color[1];
-            mColor.r = color[2];
-            mColor.a = color[3];
-            break;
-        case HAL_PIXEL_FORMAT_RGBA_8888:
-        case HAL_PIXEL_FORMAT_RGBX_8888:
-            mColor.r = color[0];
-            mColor.g = color[1];
-            mColor.b = color[2];
-            mColor.a = color[3];
-            break;
-        case HAL_PIXEL_FORMAT_RGB_565:
-            mColor.r = color[0];
-            mColor.g = color[1];
-            mColor.b = color[2];
-            mColor.a = 255;
-            break;
-        case HAL_PIXEL_FORMAT_RGB_888:
-            mColor.b = color[0];
-            mColor.g = color[1];
-            mColor.r = color[2];
-            mColor.a = 255;
-            break;
-        default:
-            MESON_LOGE("Need to expand the format(%d), check it out!", bufFormat);
-            break;
+        switch (bufFormat)
+        {
+            case HAL_PIXEL_FORMAT_BGRA_8888:
+                mColor.b = color[0];
+                mColor.g = color[1];
+                mColor.r = color[2];
+                mColor.a = color[3];
+                break;
+            case HAL_PIXEL_FORMAT_RGBA_8888:
+            case HAL_PIXEL_FORMAT_RGBX_8888:
+                mColor.r = color[0];
+                mColor.g = color[1];
+                mColor.b = color[2];
+                mColor.a = color[3];
+                break;
+            case HAL_PIXEL_FORMAT_RGB_565:
+                mColor.r = color[0];
+                mColor.g = color[1];
+                mColor.b = color[2];
+                mColor.a = 255;
+                break;
+            case HAL_PIXEL_FORMAT_RGB_888:
+                mColor.b = color[0];
+                mColor.g = color[1];
+                mColor.r = color[2];
+                mColor.a = 255;
+                break;
+            default:
+                MESON_LOGE("Need to expand the format(%d), check it out!", bufFormat);
+                break;
+        }
+
+        mFbType = DRM_FB_COLOR;
+        munmap(base, components);
+    } else {
+        MESON_LOGE("[%s]: dim layer buffer mmap fail!", __func__);
     }
 
-    MESON_LOGV("Dim layer format: %d, RGBA: (%d, %d, %d, %d)", bufFormat,
-        mColor.r, mColor.g, mColor.b, mColor.a);
-
-    mFbType = DRM_FB_COLOR;
     return HWC2_ERROR_NONE;
 }
 
