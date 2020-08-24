49eb3705e34607a7c1b2939d6d40bff42b26668e
jason <jason@khadas.com>
2020-08-23 22:03:07 -0400

    Native UI: set fb size based on osd size
    Signed-off-by: jason <jason@khadas.com>
    
    Change-Id: I2fbdd45b4bac1a9cc66ad3fed3f379468ddea631

--- common/hwc/HwcDisplayPipe.cpp
+++ common/hwc/HwcDisplayPipe.cpp
@@ -17,6 +17,10 @@
 
 #include <HwDisplayManager.h>
 
+#define HWC_BOOTED_PROP "vendor.sys.hwc.booted"
+
+
+
 HwcDisplayPipe::PipeStat::PipeStat(uint32_t id) {
     hwcId = id;
     cfg.hwcCrtcId = cfg.modeCrtcId = 0;
@@ -69,10 +73,39 @@ int32_t HwcDisplayPipe::init(std::map<uint32_t, std::shared_ptr<HwcDisplay>> & h
         /*init display pipe.*/
         updatePipe(stat);
 
+
         /* in case of composer service restart */
         if (sys_get_bool_prop(HWC_BOOTED_PROP, false)) {
             MESON_LOGD("composer service has restarted, need blank display");
             stat->hwcDisplay->blankDisplay();
+        }
+
+        sc_set_property(HWC_BOOTED_PROP, "true");
+        const char * nativeui_key = "ubootenv.var.nativeui";
+        std::string nativeui_status;
+
+
+        if (0 == sc_read_bootenv(nativeui_key, nativeui_status)) {
+             MESON_LOGE("sc_read_bootenv(%s) from uboot", nativeui_status.c_str());
+        }
+
+        if((strncmp(nativeui_status.c_str(),"enable",6)==0) && (HwcConfig::isLcdExist() == 0)) {
+             char val[PROP_VALUE_LEN_MAX];
+             std::string mode;
+             sc_get_display_mode(mode);
+             MESON_LOGI("Get hdmimode(%s) from systemcontrol service", mode.c_str());
+             if (strncmp(mode.c_str(), "2160p", 5) == 0) {
+                 if (sys_get_string_prop("ro.board.platform", val) > 0 && strcmp(val, "g12b") != 0)
+                     return 0;
+             }
+             int calibrateCoordinates[4];
+             std::string dispModeStr(mode);
+             if (0 == sc_get_osd_position(dispModeStr, calibrateCoordinates)) {
+                  fbW = calibrateCoordinates[2];
+                  fbH = calibrateCoordinates[3];
+                  MESON_LOGI("~FixedDisplayPipe HwcConfig::fbW %d fbH:%d",fbW,fbH);
+             }
+             modeMgr->setFramebufferSize(fbW, fbH);
         }
     }
 
