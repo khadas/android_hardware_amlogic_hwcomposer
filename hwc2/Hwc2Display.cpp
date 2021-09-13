94c9dfa2bbdfc32e30e2d9bd16850e822ecb8271
baocheng sun <baocheng.sun@amlogic.com>
2021-09-13 10:48:49 +0800

    hwc: fix composer crash caused by null ptr [1/1]
    PD#SWPL-59459
    
    Problem:
    Android restart during play PVR video
    
    Solution:
    Use ComposerCompositon not presentComposition when present video
    
    Verify:
    ohm
    
    Change-Id: I1eb597cbdbf73aff1c0950865e352cddbc4109ea
    Signed-off-by: baocheng sun <baocheng.sun@amlogic.com>

--- hwc2/Hwc2Display.cpp
+++ hwc2/Hwc2Display.cpp
@@ -1124,7 +1124,7 @@ hwc2_error_t Hwc2Display::presentVideo(int32_t* outPresentFence) {
         if (mValidateDisplay == false)
             mCompositionStrategy->updateComposition();
 
-        mPresentCompositionStg->commit(false);
+        mCompositionStrategy->commit(false);
     }
     return HWC2_ERROR_NONE;
 }
