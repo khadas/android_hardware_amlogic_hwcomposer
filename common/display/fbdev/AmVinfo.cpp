e7d2225fe4f9e426d5273bb2a61424480872a978
baocheng sun <baocheng.sun@amlogic.com>
2021-11-09 19:19:27 +0800

    hwc: update resolution of 768p display mode [1/1]
    PD#SWPL-52076
    
    Problem:
    display wrong ratio with 32inch(1366x768) panel size
    
    Solution:
    update resolution of 768p display mode
    
    Verify:
    am311
    
    Change-Id: Iabae2fcbc8417c4329dba2da4e82e5f2058d70b7
    Signed-off-by: baocheng sun <baocheng.sun@amlogic.com>

--- common/display/fbdev/AmVinfo.cpp
+++ common/display/fbdev/AmVinfo.cpp
@@ -353,7 +353,7 @@ static const struct vinfo_s tv_info[] = {
 	{ /* VMODE_768P_50hz */
 		.name              = "768p50hz",
 		.mode              = VMODE_768P_50HZ,
-		.width             = 1280,
+		.width             = 1366,
 		.height            = 768,
 		.field_height      = 768,
 		.aspect_ratio_num  = 16,
@@ -366,7 +366,7 @@ static const struct vinfo_s tv_info[] = {
 	{ /* VMODE_768P_60hz */
 		.name              = "768p60hz",
 		.mode              = VMODE_768P,
-		.width             = 1280,
+		.width             = 1366,
 		.height            = 768,
 		.field_height      = 768,
 		.aspect_ratio_num  = 16,
