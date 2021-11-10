224b9442ae6183e019cb26cb77e540b71bfa3fc3
dabing.qi <dabing.qi@amlogic.com>
2021-11-10 02:48:54 +0000

    hwc: T7 dual screen,support video display on external display [2/2]
    PD#SWPL-61139
    
    Problem:
    T7 dual screen not support video display on external display
    
    Solution:
    support video display on external display
    
    Verify:
    T7 in androidR
    
    Change-Id: I5509138a3c1133ad96d91fb48a7fb173001de110
    Signed-off-by: dabing.qi <dabing.qi@amlogic.com>

--- common/display/fbdev/AmFramebuffer.h
+++ common/display/fbdev/AmFramebuffer.h
@@ -29,7 +29,10 @@ enum {
     OSD_AFBC          = (1 << 5),
     OSD_VIU2          = (1 << 29),
     OSD_VIU1          = (1 << 30),
-    OSD_LAYER_ENABLE  = (1 << 31)
+    OSD_LAYER_ENABLE  = (1 << 31),
+    VIDEO_PANEL_1     = (1 << 18),
+    VIDEO_PANEL_2     = (1 << 20),
+    VIDEO_PANEL_3     = (1 << 22),
 };
 
 /*video plane caps.*/
--- common/display/include/HwcVideoPlane.h
+++ common/display/include/HwcVideoPlane.h
@@ -46,6 +46,7 @@ protected:
 protected:
     int32_t mDrvFd;
     uint32_t mId;
+    uint32_t mComposerNum;
     int32_t mCapability;
 
     char mName[64];
