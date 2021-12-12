c2da87bd378468545d8fb89c242ffbade694934a
Tianhua Sun <tianhua.sun@amlogic.com>
2021-12-12 21:02:46 +0800

    hwc: implement VtDisplayObserver for hwc2Display [1/1]
    PD#SWPL-75842
    
    Problem:
    need to inform display when enable gamemode.
    
    Solution:
    display will define VtDisplayObserver and register it to
    layer.
    
    Verify:
    T3 and T7
    
    Change-Id: I18e07077f92a715e6067bf4bb564f22877e654f7
    Signed-off-by: Tianhua Sun <tianhua.sun@amlogic.com>

--- hwc2/Hwc2Display.h
+++ hwc2/Hwc2Display.h
@@ -49,7 +49,7 @@ public:
 };
 
 class Hwc2Display
-    : public HwcDisplay, public HwcVsyncObserver {
+    : public HwcDisplay, public HwcVsyncObserver, public VtDisplayObserver {
 public:
     Hwc2Display(std::shared_ptr<Hwc2DisplayObserver> observer, uint32_t display);
     virtual ~Hwc2Display();
@@ -153,6 +153,8 @@ public:
     /* recieve videotunnel cmds */
     virtual int recieveVtCmds();
     virtual bool newGameBuffer();
+    virtual void onFrameAvailable();
+    virtual void onVtVideoGameMode(bool enable);
 
     virtual void refreshVtLayers();
 
@@ -264,6 +266,7 @@ protected:
     /* for video tunnel mode video*/
     std::shared_ptr<VideoTunnelThread> mVideoTunnelThread;
     std::shared_ptr<HwcVsync> mVtVsync;
+    bool mGameMode;
     bool mVtVsyncStatus;
     bool mDisplayConnection;
     bool mOutsideChanged;
