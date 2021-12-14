f1d938c893e9514f57615a04c3b216a64aafb8fc
baocheng sun <baocheng.sun@amlogic.com>
2021-12-14 20:34:38 +0800

    hwc: fix a nullptr crash when switch cvbs to hdmi [1/1]
    PD#SWPL-67769
    
    Problem:
    hwc crash caused by a nullptr
    
    Solution:
    hold the vt mutex when update display resource
    
    Verify:
    ohm
    
    Change-Id: Ia99d3e321201f04703664050ad72a3e4fab2bfdf
    Signed-off-by: baocheng sun <baocheng.sun@amlogic.com>

--- composition/simplestrategy/MultiplanesWithDiComposition/MultiplanesWithDiComposition.cpp
+++ composition/simplestrategy/MultiplanesWithDiComposition/MultiplanesWithDiComposition.cpp
@@ -1340,6 +1340,11 @@ int MultiplanesWithDiComposition::commit(bool sf) {
     ATRACE_CALL();
     std::lock_guard<std::mutex> lock(mMutex);
 
+    if (mCrtc == nullptr) {
+        MESON_LOGE("commit but no crtc!");
+        return HWC2_ERROR_NO_RESOURCES;
+    }
+
     /* replace composer output with din0 Pair. */
     std::shared_ptr<DrmFramebuffer> composerOutput;
     bool setPlaneSuccess = true;
