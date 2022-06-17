e13cfbc55ac073c276b94fb1b69546e041457ca2
En Liu <en.liu@amlogic.com>
2022-06-16 19:57:28 -0700

    hwc: use soong_config_ macros to set Soong config variables [1/1]
    PD#SWPL-85478
    
    Problem:
    hwcomposer branch out for Android S
    
    Solution:
    Use soong_config_ macros to set Soong config variables.
    Add hardware_amlogic_hwcomposer_license.
    
    Verify:
    ohm
    
    Change-Id: I3a4c115e26c808912ab8400d05469cb4e1d7843a
    Signed-off-by: En Liu <en.liu@amlogic.com>

--- hwcomposer.device.mk
+++ hwcomposer.device.mk
@@ -111,76 +111,37 @@ endif
 
 # Setup configuration in Soong namespace
 #
-SOONG_CONFIG_NAMESPACES += meson_hwc
-SOONG_CONFIG_meson_hwc += \
-    hwc_release \
-    hwc_backend \
-    display_num \
-    primary_fb_width \
-    primary_fb_height \
-    extend_fb_width \
-    extend_fb_height \
-    primary_connector_type \
-    extend_connector_type \
-    enforces_max_refresh_rate \
-    enable_headless_mode \
-    enable_software_vsync \
-    enable_primary_hotplug \
-    enable_secure_layer_process \
-    disable_cursor_plane \
-    enable_keystone_correction \
-    enable_ge2d_composition \
-    hdmi_frac_mode \
-    hwc_pipeline \
-    android_version_s \
-    vdin_fbprocessor \
-    enable_active_mode \
-    enable_real_mode \
-    enable_pre_display_calibrate \
-    target_use_default_hdr_property \
-    target_app_layer_use_continuous_buffer \
-    pipe_viu1vdinviu2_always_loopback \
-    dynamic_switch_connector \
-    dynamic_swich_viu \
-    enable_video_aisr \
-    enable_video_aipq \
-    enable_vt_hwVsync \
-    enable_uvm_dettach
-
-SOONG_CONFIG_meson_hwc_hwc_release := $(HWC_RELEASE)
-SOONG_CONFIG_meson_hwc_hwc_backend := $(HWC_BACKEND)
-SOONG_CONFIG_meson_hwc_display_num := $(HWC_DISPLAY_NUM)
-SOONG_CONFIG_meson_hwc_primary_fb_width := $(HWC_PRIMARY_FRAMEBUFFER_WIDTH)
-SOONG_CONFIG_meson_hwc_primary_fb_height := $(HWC_PRIMARY_FRAMEBUFFER_HEIGHT)
-SOONG_CONFIG_meson_hwc_enforces_max_refresh_rate := $(HWC_ENFORCES_MAX_REFRESH_RATE)
-SOONG_CONFIG_meson_hwc_extend_fb_width := $(HWC_EXTEND_FRAMEBUFFER_WIDTH)
-SOONG_CONFIG_meson_hwc_extend_fb_height := $(HWC_EXTEND_FRAMEBUFFER_HEIGHT)
-SOONG_CONFIG_meson_hwc_primary_connector_type := $(HWC_PRIMARY_CONNECTOR_TYPE)
-SOONG_CONFIG_meson_hwc_extend_connector_type := $(HWC_EXTEND_CONNECTOR_TYPE)
-SOONG_CONFIG_meson_hwc_enable_headless_mode := $(HWC_ENABLE_HEADLESS_MODE)
-SOONG_CONFIG_meson_hwc_enable_software_vsync := $(HWC_ENABLE_SOFTWARE_VSYNC)
-SOONG_CONFIG_meson_hwc_enable_primary_hotplug := $(HWC_ENABLE_PRIMARY_HOTPLUG)
-SOONG_CONFIG_meson_hwc_enable_secure_layer_process := $(HWC_ENABLE_SECURE_LAYER_PROCESS)
-SOONG_CONFIG_meson_hwc_disable_cursor_plane := $(HWC_DISABLE_CURSOR_PLANE)
-SOONG_CONFIG_meson_hwc_enable_keystone_correction := $(HWC_ENABLE_KEYSTONE_CORRECTION)
-SOONG_CONFIG_meson_hwc_enable_ge2d_composition := $(HWC_ENABLE_GE2D_COMPOSITION)
-SOONG_CONFIG_meson_hwc_enable_display_mode_management := $(HWC_ENABLE_DISPLAY_MODE_MANAGEMENT)
-SOONG_CONFIG_meson_hwc_hdmi_frac_mode := $(HWC_HDMI_FRAC_MODE)
-SOONG_CONFIG_meson_hwc_hwc_pipeline := $(HWC_PIPELINE)
-SOONG_CONFIG_meson_hwc_vdin_fbprocessor := $(HWC_VDIN_FBPROCESSOR)
-SOONG_CONFIG_meson_hwc_enable_active_mode := $(HWC_ENABLE_ACTIVE_MODE)
-SOONG_CONFIG_meson_hwc_enable_real_mode := $(HWC_ENABLE_REAL_MODE)
-SOONG_CONFIG_meson_hwc_enable_pre_display_calibrate := $(HWC_ENABLE_PRE_DISPLAY_CALIBRATE)
-SOONG_CONFIG_meson_hwc_target_use_default_hdr_property := $(HWC_ENABLE_DEFAULT_HDR_CAPABILITIES)
-SOONG_CONFIG_meson_hwc_target_app_layer_use_continuous_buffer := $(TARGET_APP_LAYER_USE_CONTINUOUS_BUFFER)
-SOONG_CONFIG_meson_hwc_pipe_viu1vdinviu2_always_loopback := $(HWC_PIPE_VIU1VDINVIU2_ALWAYS_LOOPBACK)
-SOONG_CONFIG_meson_hwc_dynamic_switch_connector := $(HWC_DYNAMIC_SWITCH_CONNECTOR)
-SOONG_CONFIG_meson_hwc_dynamic_swich_viu := $(HWC_DYNAMIC_SWITCH_VIU)
-SOONG_CONFIG_meson_hwc_android_version_s := $(HWC_ANDROID_S)
-SOONG_CONFIG_meson_hwc_enable_video_aisr := $(HWC_VIDEO_AISR)
-SOONG_CONFIG_meson_hwc_enable_video_aipq := $(HWC_VIDEO_AIPQ)
-SOONG_CONFIG_meson_hwc_enable_vt_hwVsync := $(HWC_VT_HW_VSYNC)
-SOONG_CONFIG_meson_hwc_enable_uvm_dettach := $(HWC_UVM_DETTACH)
+$(call soong_config_set,meson_hwc,hwc_release,$(HWC_RELEASE))
+$(call soong_config_set,meson_hwc,display_num,$(HWC_DISPLAY_NUM))
+$(call soong_config_set,meson_hwc,primary_fb_width,$(HWC_PRIMARY_FRAMEBUFFER_WIDTH))
+$(call soong_config_set,meson_hwc,primary_fb_height,$(HWC_PRIMARY_FRAMEBUFFER_HEIGHT))
+$(call soong_config_set,meson_hwc,enforces_max_refresh_rate,$(HWC_ENFORCES_MAX_REFRESH_RATE))
+$(call soong_config_set,meson_hwc,extend_fb_width,$(HWC_EXTEND_FRAMEBUFFER_WIDTH))
+$(call soong_config_set,meson_hwc,extend_fb_height,$(HWC_EXTEND_FRAMEBUFFER_HEIGHT))
+$(call soong_config_set,meson_hwc,primary_connector_type,$(HWC_PRIMARY_CONNECTOR_TYPE))
+$(call soong_config_set,meson_hwc,extend_connector_type,$(HWC_EXTEND_CONNECTOR_TYPE))
+$(call soong_config_set,meson_hwc,enable_headless_mode,$(HWC_ENABLE_HEADLESS_MODE))
+$(call soong_config_set,meson_hwc,enable_software_vsync,$(HWC_ENABLE_SOFTWARE_VSYNC))
+$(call soong_config_set,meson_hwc,enable_primary_hotplug,$(HWC_ENABLE_PRIMARY_HOTPLUG))
+$(call soong_config_set,meson_hwc,enable_secure_layer_process,$(HWC_ENABLE_SECURE_LAYER_PROCESS))
+$(call soong_config_set,meson_hwc,disable_cursor_plane,$(HWC_DISABLE_CURSOR_PLANE))
+$(call soong_config_set,meson_hwc,enable_keystone_correction,$(HWC_ENABLE_KEYSTONE_CORRECTION))
+$(call soong_config_set,meson_hwc,enable_ge2d_composition,$(HWC_ENABLE_GE2D_COMPOSITION))
+$(call soong_config_set,meson_hwc,enable_display_mode_management,$(HWC_ENABLE_DISPLAY_MODE_MANAGEMENT))
+$(call soong_config_set,meson_hwc,hdmi_frac_mode,$(HWC_HDMI_FRAC_MODE))
+$(call soong_config_set,meson_hwc,hwc_pipeline,$(HWC_PIPELINE))
+$(call soong_config_set,meson_hwc,enable_active_mode,$(HWC_ENABLE_ACTIVE_MODE))
+$(call soong_config_set,meson_hwc,enable_real_mode,$(HWC_ENABLE_REAL_MODE))
+$(call soong_config_set,meson_hwc,enable_pre_display_calibrate,$(HWC_ENABLE_PRE_DISPLAY_CALIBRATE))
+$(call soong_config_set,meson_hwc,target_use_default_hdr_property,$(HWC_ENABLE_DEFAULT_HDR_CAPABILITIES))
+$(call soong_config_set,meson_hwc,pipe_viu1vdinviu2_always_loopback,$(HWC_PIPE_VIU1VDINVIU2_ALWAYS_LOOPBACK))
+$(call soong_config_set,meson_hwc,dynamic_switch_connector,$(HWC_DYNAMIC_SWITCH_CONNECTOR))
+$(call soong_config_set,meson_hwc,dynamic_swich_viu,$(HWC_DYNAMIC_SWITCH_VIU))
+$(call soong_config_set,meson_hwc,android_version_s,$(HWC_ANDROID_S))
+$(call soong_config_set,meson_hwc,enable_video_aisr,$(HWC_VIDEO_AISR))
+$(call soong_config_set,meson_hwc,enable_vt_hwVsync,$(HWC_VT_HW_VSYNC))
+$(call soong_config_set,meson_hwc,enable_video_aipq,$(HWC_VIDEO_AIPQ))
+$(call soong_config_set,meson_hwc,enable_uvm_dettach,$(HWC_UVM_DETTACH))
 
 #$(warning "the value of version_s: $(HWC_ANDROID_S)")
 $(warning "the value of uvm_dettach: $(HWC_UVM_DETTACH)")
