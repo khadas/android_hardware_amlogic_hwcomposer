e318f0a811d2701b9c3eb1397ba6c778becd845a
baocheng sun <baocheng.sun@amlogic.com>
2023-09-21 18:42:17 +0800

    mesondisplay: move mesondisplay code from hwc to mesondisplay git [1/3]
    PD#SWPL-139439
    
    Problem:
    enable mesondisplay git to share code between android and linux
    
    Solution:
    move mesondisplay code from hwc to mesondisplay git
    
    Verify:
    ohm
    
    Change-Id: Icdf1661cdeff09c1d6c2535cba23731703e2172b
    Signed-off-by: baocheng sun <baocheng.sun@amlogic.com>

--- mesondisplay/common/base/DrmFrameBufferBase.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2023 Amlogic, Inc. All rights reserved.
- *
- * This source code is subject to the terms and conditions defined in the
- * file 'LICENSE' which is part of this source code package.
- *
- * Description:
- */
-
