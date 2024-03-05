ba3276cb40a9a1a2765366c34641eb7a35e5cd1f
fuqing.chen <fuqing.chen@amlogic.com>
2024-03-05 03:11:16 +0000

    hwc: fix deadlock when setmode by mesondisplay [1/1]
    PD#SWPL-159201
    
    Problem:
    validate display and set mode by mesondisplay
    result deadlock
    
    Solution:
    mConnectorType updated when display pipe init or
    update
    
    Verify:
    T3X
    
    Change-Id: I8aaea4a53fd487023ab0ab1beebd7e95689849d1
    Signed-off-by: fuqing.chen <fuqing.chen@amlogic.com>

--- hwc2/Hwc2Display.h
+++ hwc2/Hwc2Display.h
@@ -276,7 +276,6 @@ protected:
     std::shared_ptr<HwDisplayCrtc> mCrtc;
     std::shared_ptr<HwDisplayConnector> mConnector;
     std::vector<std::shared_ptr<HwDisplayPlane>> mPlanes;
-    bool mConnectorUpdated;
     drm_connector_type_t mConnectorType;
 
     /*composition related components*/
