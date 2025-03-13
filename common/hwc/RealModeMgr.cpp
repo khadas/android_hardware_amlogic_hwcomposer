ffb55d974563607193cea0f91e14a600bf336728
Xiong Zhang <xiong.zhang@wesion.com>
2025-03-13 10:01:12 +0800

    hdmitx: completion vesa resolutions
    bootloader/uboot [1/3]
    common/common14-5.15/common/common_drivers [2/3]
    hardware/amlogic/hwcomposer [3/3]
    
    Change-Id: I9456769427cd54cf6378f3762065a815be6b76ff
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- common/hwc/RealModeMgr.cpp
+++ common/hwc/RealModeMgr.cpp
@@ -83,6 +83,27 @@ static const char* DISPLAY_MODE_LIST[] = {
     "7680x4320p48hz",    // MODE_8K4K48HZ
     "7680x4320p50hz",    // MODE_8K4K50HZ
     "7680x4320p60hz",    // MODE_8K4K60HZ
+    "480x320p60hz"
+    "640x480p60hz"
+    "800x480p60hz"
+    "800x600p60hz"
+    "1024x600p60hz"
+    "1024x768p60hz"
+    "1280x480p60hz"
+    "1280x800p60hz"
+    "1280x1024p60hz"
+    "1360x768p60hz"
+    "1440x900p60hz"
+    "1600x900p60hz"
+    "1600x1200p60hz"
+    "1680x1050p60hz"
+    "1920x720p60hz"
+    "1920x1200p60hz"
+    "1920x1400p60hz"
+    "2560x1080p60hz"
+    "2560x1440p60hz"
+    "2560x1600p60hz"
+    "3440x1440p60hz"
     "panel",        // MODE_PANEL
     "pal_m",        // MODE_PAL_M
     "pal_n",        // MODE_PAL_N
